ea07828184e00ff37e2c5f5e13b3557ef9b1b65a
OlajuwonX <olasimboolajuwon@gmail.com>
2025-08-14 13:47:45 +0100

    after creating the sent message logic

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,9 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
+
+// Logic for the authenticated users sidebar and get messages between users.
 export const usersSidebar = async (req, res) => {
 try{
     const loggedInUserId = req.user._id; //user again because the route is protected by the protectRoute logic.
@@ -11,5 +15,56 @@ try{
     res.status(500).json({error: "Internal Server Error"});
 }
 }
+
+export const getMessages = async (req, res) => {
+    try{
+        const {id:usersChatId}=req.params; //we will use dynamic values, and it is id because that was what we called it in the message routes {:id}. so we are renaming it to usersChatId.
+        const myId = req.user._id; //id of the current user.
+
+        const messages =await Message.find({
+            $or: [
+                {senderId: myId, receiverId: usersChatId},
+                {senderId: usersChatId, receiverId: myId}
+            ]
+        }); //this is to basically get the messages between the two users, either the one initiated by the sender or the one initiated by the receiver. MyId is the current user_id.
+
+        res.status(200).json(messages);
+    } catch (e) {
+        console.error("Error in getMessages",e.messages);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+// Logic for the receiving end of the messages sent from the currentId.
+export const sentMessage = async (req, res) => {
+    try{
+        const {text, image} = req.params;
+        const {id:receiverId}=req.params; //renaming this dynamic id as receiver_id, same way we did with my_id.
+        const senderId = req.user._id;//current user id.
+
+        let imageUrl; //this is in case the message is an image.
+        if (image) {
+            //Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload({image});
+            imageUrl = uploadResponse.secure.url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        }); // this is to return the new message.
+
+        await newMessage.save(); //this is to save the new message.
+
+        //to do, implement realtime functionality. => via socket.io
+
+        res.status(201).json(newMessage);
+    } catch (e) {
+        console.error("Error in sendMessages",e.messages);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+} //the message sent could either be a text or an image.
 
 //this is basically to fetch all users but not show ourselves in the contacts list.
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import {protectRoute} from "../middleware/protectRoute.js";
-import {usersSidebar} from "../controllers/message.controller.js";
+import {getMessages, sentMessage, usersSidebar} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+// Endpoints for the authenticated users sidebar and messages between users.
 router.get("/users", protectRoute, usersSidebar); //this will fetch the users that will be on the sidebar.
+router.get("/:id", protectRoute, getMessages); //this is to get the messages between two distinct users/the whole chat between two different users.
+
+
+// Endpoint for the receiver of the message.
+router.post("/sender:id", protectRoute, sentMessage); //this is for the person receiving the message.
+
 
 export default router;

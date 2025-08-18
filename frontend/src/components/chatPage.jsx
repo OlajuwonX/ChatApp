b4a89f80affe60ad826569615611862ad816c03b
OlajuwonX <olasimboolajuwon@gmail.com>
2025-08-18 18:39:48 +0100

    after reshuffling the themes

--- frontend/src/components/chatPage.jsx
+++ frontend/src/components/chatPage.jsx
@@ -6,7 +6,7 @@ import ChatContainer from "./ChatContainer.jsx";
 const ChatPage = () => {
     const {selectedUser} = useChatStore();
     return (
-        <div className="h-full bg-base-200">
+        <div className="h-screen bg-base-200">
             <div className="flex items-center justify-center pt-15 px-4">
                 <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
                     <div className="flex h-full rounded-lg overflow-hidden">
